a7b1746f30c477cfd122779a0b1b4c0cf9ebbaa3
ripionck <ripion.chakmar@gmail.com>
2024-12-12 18:28:02 +0600

    added .env, .ignore file

--- internal/token/token.go
+++ internal/token/token.go
@@ -23,7 +23,7 @@ type SignedDetails struct {
 }
 
 var UserData *mongo.Collection = datasource.UserData(datasource.Client, "Users")
-var SECRET_KEY = os.Getenv("SECRET_LOVE")
+var SECRET_KEY = os.Getenv("SECRET_KEY")
 
 func TokenGenerator(email string, firstname string, lastname string, uid string) (signedtoken string, signedrefreshtoken string, err error) {
 	claims := &SignedDetails{
