1d6e26ee04dd012ce79a656536a02b85816c6df4
ripionck <ripion.chakmar@gmail.com>
2024-12-12 18:46:57 +0600

    added cart and address controllers

--- cmd/aevum-emporium-be/main.go
+++ cmd/aevum-emporium-be/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"aevum-emporium-be/internal/controllers"
+	"aevum-emporium-be/internal/datasource"
+	"aevum-emporium-be/internal/routes"
+
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	app := controllers.NewApplication(datasource.ProductData(datasource.Client, "Products"), datasource.UserData(datasource.Client, "Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	// router.Use(middleware.Authentication())
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	// router.GET("/listcart", controllers.GetItemFromCart())
+	// router.POST("/addaddress", controllers.AddAddress())
+	// router.PUT("/edithomeaddress", controllers.EditHomeAddress())
+	// router.PUT("/editworkaddress", controllers.EditWorkAddress())
+	// router.GET("/deleteaddresses", controllers.DeleteAddress())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+	log.Fatal(router.Run(":" + port))
+}
