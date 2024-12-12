37860d5bfbb57a89b7395cd4881a6a188c44587b
ripionck <ripion.chakmar@gmail.com>
2024-12-12 18:21:19 +0600

    initial project setup

---
+++ internal/datasource/db.go
@@ -0,0 +1,40 @@
+package datasource
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSet() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://development:testpassword@localhost:27017"))
+	if err != nil {
+		log.Fatalf("Error creating MongoDB client: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
+	fmt.Println("Connected to MongoDB")
+	return client
+}
+
+var Client *mongo.Client = DBSet()
+
+func UserData(client *mongo.Client, CollectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("aevum-emporium").Collection(CollectionName)
+	return collection
+
+}
+
+func ProductData(client *mongo.Client, CollectionName string) *mongo.Collection {
+	var productcollection *mongo.Collection = client.Database("aevum-emporium").Collection(CollectionName)
+	return productcollection
+}
